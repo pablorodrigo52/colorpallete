72d2eca1c72cf5776612aa661a1cd4cf4b19f555
Pablo Rodrigo <prlsantos@dcomp.ufs.br>
2020-08-15 19:18:57 -0300

    Updates

---
+++ precache-manifest.d7cc7890757d8ffe81c47d9767818244.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2be3880be9e5918f1d8159225873c8a",
+    "url": "/pablorodrigo52/colorpallete/index.html"
+  },
+  {
+    "revision": "87aa6b0e83bea65e9b7c",
+    "url": "/pablorodrigo52/colorpallete/static/css/main.d5062221.chunk.css"
+  },
+  {
+    "revision": "f8763fe8358ef6ea0ecc",
+    "url": "/pablorodrigo52/colorpallete/static/js/2.ee387b63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pablorodrigo52/colorpallete/static/js/2.ee387b63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87aa6b0e83bea65e9b7c",
+    "url": "/pablorodrigo52/colorpallete/static/js/main.83cf693e.chunk.js"
+  },
+  {
+    "revision": "e9baffef5671c1b411fe",
+    "url": "/pablorodrigo52/colorpallete/static/js/runtime-main.730391c9.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/pablorodrigo52/colorpallete/precache-manifest.d7cc7890757d8ffe81c47d9767818244.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pablorodrigo52/colorpallete/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
